0bc2911f22eb862a74f88d91a5460c6b5c161cfb
collijk <collijk@uw.edu>
2023-10-29 13:17:28 -0700

    interface

--- src/geofuse/shapes/__init__.py
+++ src/geofuse/shapes/__init__.py
@@ -1,2 +1,3 @@
 from geofuse.shapes.merge import collapse_mergeable_geoms, determine_mergeability
 from geofuse.shapes.partition import partition_geoms
+from geofuse.shapes.clean import fix_multipolygons, fix_overlapping_geometries
